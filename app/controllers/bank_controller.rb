3d29c21a990c91e96cc36c6a77dd6232d0df707e
sitarane <julien@larevolution.re>
2022-10-08 16:18:07 +0200

    add controller logic

--- app/controllers/bank_controller.rb
+++ app/controllers/bank_controller.rb
@@ -1,6 +1,24 @@
 class BankController < ApplicationController
   def show
-    #ystart_date =
-    @credit = 100
+    date_of_birth = Date.new(2013, 07, 04)
+    allowance = 0.5 # in Euros per year of age per week
+    start_date = Date.new(2020, 07)
+
+    @credit = compute_credit(date_of_birth, allowance, start_date)
+  end
+
+  private
+
+  def compute_credit(date_of_birth, allowance, start_date)
+    credit = 0
+    (start_date..Date.today).select{ |date| date.wday == 5 }.each do | date |
+       age = date.year - date_of_birth.year
+       age -= 1 if date < date_of_birth + age.years # for days before birthday
+       allowance_for_this_week = age * allowance
+       ## For debug:
+       # puts "date is #{date}, Damien is #{age}, he gets #{allowance_for_this_week} added to his stash of #{credit}"
+       credit += allowance_for_this_week
+    end
+    credit / 2 # Elsy gives him half
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root 'bank#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'bank#show'
 end
