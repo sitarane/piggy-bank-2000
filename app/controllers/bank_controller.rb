98cd2c0495d4c6814f1f209de6b00f8d42b84058
sitarane <julien@larevolution.re>
2022-10-08 16:34:30 +0200

    style the bank

--- app/controllers/bank_controller.rb
+++ app/controllers/bank_controller.rb
@@ -11,7 +11,8 @@ class BankController < ApplicationController
 
   def compute_credit(date_of_birth, allowance, start_date)
     credit = 0
-    (start_date..Date.today).select{ |date| date.wday == 5 }.each do | date |
+    ## He gets his money on Saturday date.wday == 6
+    (start_date..Date.today).select{ |date| date.wday == 6 }.each do | date |
        age = date.year - date_of_birth.year
        age -= 1 if date < date_of_birth + age.years # for days before birthday
        allowance_for_this_week = age * allowance
