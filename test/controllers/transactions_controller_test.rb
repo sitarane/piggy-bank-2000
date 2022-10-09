1b823f6396a9c4a69fdc5de900bbf39fe8a4ad42
sitarane <julien@larevolution.re>
2022-10-09 10:50:08 +0200

    fix up transaction controller tests

--- test/controllers/transactions_controller_test.rb
+++ test/controllers/transactions_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class TransactionsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @transaction = transactions(:one)
+    @transaction = transactions(:withdrawal_7)
   end
 
   test "should get index" do
@@ -11,13 +11,20 @@ class TransactionsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
-    get new_transaction_url
+    get new_transaction_url, params: { transaction: { deposit: false, } }
     assert_response :success
   end
 
   test "should create transaction" do
     assert_difference("Transaction.count") do
-      post transactions_url, params: { transaction: { amount: @transaction.amount, deposit: @transaction.deposit, executor: @transaction.executor } }
+      post transactions_url, params: {
+        transaction: {
+          amount: @transaction.amount,
+          comment: @transaction.comment,
+          deposit: @transaction.deposit,
+          executor: @transaction.executor
+          }
+        }
     end
 
     assert_redirected_to transaction_url(Transaction.last)
@@ -28,21 +35,11 @@ class TransactionsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get edit" do
-    get edit_transaction_url(@transaction)
-    assert_response :success
-  end
-
-  test "should update transaction" do
-    patch transaction_url(@transaction), params: { transaction: { amount: @transaction.amount, deposit: @transaction.deposit, executor: @transaction.executor } }
-    assert_redirected_to transaction_url(@transaction)
-  end
-
-  test "should destroy transaction" do
-    assert_difference("Transaction.count", -1) do
-      delete transaction_url(@transaction)
-    end
-
-    assert_redirected_to transactions_url
-  end
+  # test "should destroy transaction" do
+  #   assert_difference("Transaction.count", -1) do
+  #     delete transaction_url(@transaction)
+  #   end
+  #
+  #   assert_redirected_to transactions_url
+  # end
 end
