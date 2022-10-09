31852ed4f97c656bc7ea332dfc9838cd33ac65d8
sitarane <julien@larevolution.re>
2022-10-09 14:11:38 +0200

    fix RSOD in prod, add prod environment var

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,4 +72,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # RSOD told me to do this
+  config.hosts << "bbb.kazrobo.com"
 end
